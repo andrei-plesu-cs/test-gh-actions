71b74a849d65e98e5f2cbc1e0b7c2d6ea21a9ac9
Andrei-Bogdan PLEŞOIANU (94849) <andrei.plesoianu@stud.acs.upb.ro>
2026-09-25 12:51:16 +0300

    Upload test report as artifact

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,5 +7,11 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
+    reporters: process.env.CI
+      ? ['default', 'junit']
+      : ['default'],
+    outputFile: {
+      junit: './test-results/junit.xml',
+    },
   },
 })
